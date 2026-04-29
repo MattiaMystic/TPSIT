3c0703c407b3f058b73cb1eac58d1fcfc9b191fd
MattiaMystic <150335971+MattiaMystic@users.noreply.github.com>
2026-04-29 05:20:13 +0200

    Update Client.py

--- 5F_2025-2026/Python/Studenti_Media/Client.py
+++ 5F_2025-2026/Python/Studenti_Media/Client.py
@@ -2,62 +2,66 @@ import socket
 
 def prendi_input():
     nome = input("Nome studente: ")
-    voti = input("Inserisci voti separati da virgola: ")
-
+    voti = input ("Voti studente separati da una virgola:")
+    
     lista = voti.split(",")
-
-    voti_validi = []
-
+    
+    voti_validi= []
+    
     for v in lista:
-        voto = float(v)
+        voto=float(v)
         voti_validi.append(voto)
-
-    return nome, voti_validi
-
+    
+    return nome,voti_validi
 
 def mostra_risultato(risposta):
     dati = risposta.split(",")
-
-    print("\n--- RISULTATO ---")
-    print("Studente:", dati[0])
-    print("Media:", dati[1])
-    print("Esito:", dati[2])
-    print("Min:", dati[3])
-    print("Max:", dati[4])
-
-
+    
+    print("\n--RISULTATO--")
+    print("Nome studente: ",dati[0])
+    print("Media studente: ",dati[1])
+    print("Esito: ",dati[2])
+    print("Voto massimo: ",dati[3])
+    print("Voto minimo: ",dati[4])
+    
+    
 def start_client():
     while True:
-        print("\n1. Calcola voti")
-        print("2. Esci")
-
-        scelta = input("Scelta: ")
-
+        print("\n1.Calcola voti")
+        print("2.Exit")
+        
+        scelta=input("Scelta: ")
+        
         if scelta == "1":
-            nome, voti = prendi_input()
-
-            voti_str = ""
+            nome,voti=prendi_input()
+            
+            voti_str=""
             for i in range(len(voti)):
-                voti_str = voti_str + str(voti[i])
-                if i < len(voti) - 1:
-                    voti_str = voti_str + ","
-
+                voti_str=voti_str+str(voti[i])
+                
+                if i< len(voti)-1:
+                    voti_str=voti_str+","
+            
             messaggio = nome + ";" + voti_str
-
-            client = socket.socket()
-            client.connect(("localhost", 12345))
-
+    
+    
+            client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            client.connect(("127.0.0.1",12345))
+            
             client.send(messaggio.encode())
-
+            
             risposta = client.recv(1024).decode()
-
+            
             client.close()
-
+            
             mostra_risultato(risposta)
-
-        elif scelta == "2":
+        
+        elif scelta=="2":
+            print("USCITA")
             break
-
 
 if __name__ == "__main__":
     start_client()
+    
+    
+        
